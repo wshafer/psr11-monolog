115afba8a01be4a604cb0ceb08d25b57ec3f6f3a
Westin Shafer <westin_shafer@comcast.com>
2017-07-01 21:15:43 -0600

    Fix Styles

--- src/Handler/NativeMailerHandlerFactory.php
+++ src/Handler/NativeMailerHandlerFactory.php
@@ -11,13 +11,13 @@ class NativeMailerHandlerFactory implements FactoryInterface
 {
     public function __invoke(array $options)
     {
-        $to = (array) $options['to'] ?? true;
+        $toEmail = (array) $options['to'] ?? true;
         $subject = (string) $options['subject'] ?? true;
-        $from = (string) $options['from'] ?? true;
+        $fromEmail = (string) $options['from'] ?? true;
         $level = $options['level'] ?? Logger::DEBUG;
         $bubble = (boolean) $options['bubble'] ?? true;
         $maxColumnWidth = (int) $options['maxColumnWidth'] ?? 70;
 
-        return new NativeMailerHandler($to, $subject, $from, $level, $bubble, $maxColumnWidth);
+        return new NativeMailerHandler($toEmail, $subject, $fromEmail, $level, $bubble, $maxColumnWidth);
     }
 }
