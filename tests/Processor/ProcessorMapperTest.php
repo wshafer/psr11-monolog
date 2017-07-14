bb4cd8980f54641a1a4e25513290efe2e3af5a08
wshafer <shafer_w2002@yahoo.com>
2017-07-14 09:06:23 -0600

    Adds PSRLogMessageProcessor Update Readme Fix Handler config to allow for multiple formatters.

---
+++ tests/Processor/ProcessorMapperTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace WShafer\PSR11MonoLog\Test\Handler;
+
+use PHPUnit\Framework\TestCase;
+
+use WShafer\PSR11MonoLog\Processor\ProcessorMapper;
+use WShafer\PSR11MonoLog\Processor\PsrLogMessageProcessorFactory;
+
+/**
+ * @covers \WShafer\PSR11MonoLog\Processor\ProcessorMapper
+ */
+class ProcessorMapperTest extends TestCase
+{
+    /** @var ProcessorMapper */
+    protected $mapper;
+
+    public function setup()
+    {
+        $this->mapper = new ProcessorMapper();
+    }
+
+    public function testPsrLogMessage()
+    {
+        $expected = PsrLogMessageProcessorFactory::class;
+        $result = $this->mapper->map('psrLogMessage');
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testMapNotFound()
+    {
+        $result = $this->mapper->map('notHere');
+        $this->assertNull($result);
+    }
+}
---
+++ tests/Processor/PsrLogMessageProcessorFactoryTest.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace WShafer\PSR11MonoLog\Test\Handler;
+
+use Monolog\Processor\PsrLogMessageProcessor;
+use PHPUnit\Framework\TestCase;
+use WShafer\PSR11MonoLog\Processor\PsrLogMessageProcessorFactory;
+
+/**
+ * @covers \WShafer\PSR11MonoLog\Processor\PsrLogMessageProcessorFactory
+ */
+class PsrLogMessageProcessorFactoryTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $factory = new PsrLogMessageProcessorFactory();
+        $handler = $factory([]);
+        $this->assertInstanceOf(PsrLogMessageProcessor::class, $handler);
+    }
+}
