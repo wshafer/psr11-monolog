43827529268ba1ece5c111bfb6b7dc98e74c5a59
wshafer <shafer_w2002@yahoo.com>
2017-07-14 15:20:08 -0600

    Adds IntrospectionProcessor

---
+++ src/Processor/IntrospectionProcessorFactory.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace WShafer\PSR11MonoLog\Processor;
+
+use Monolog\Processor\IntrospectionProcessor;
+use WShafer\PSR11MonoLog\FactoryInterface;
+
+class IntrospectionProcessorFactory implements FactoryInterface
+{
+    public function __invoke(array $options)
+    {
+        $level          = (int)   ($options['level']               ?? Logger::DEBUG);
+        $skipPartials   = (array) ($options['skipClassesPartials'] ?? []);
+        $skipFrameCount = (int)   ($options['skipStackFramesCount'] ?? 0);
+
+        return new IntrospectionProcessor(
+            $level,
+            $skipPartials,
+            $skipFrameCount
+        );
+    }
+}
---
+++ tests/Processor/IntrospectionProcessorFactoryTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace WShafer\PSR11MonoLog\Test\Handler;
+
+use Monolog\Logger;
+use Monolog\Processor\IntrospectionProcessor;
+use PHPUnit\Framework\TestCase;
+use WShafer\PSR11MonoLog\Processor\IntrospectionProcessorFactory;
+
+/**
+ * @covers \WShafer\PSR11MonoLog\Processor\PsrLogMessageProcessorFactory
+ */
+class IntrospectionProcessorFactoryTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $options = [
+            'level'                => Logger::INFO,
+            'skipClassesPartials'  => [],
+            'skipStackFramesCount' => 0
+        ];
+
+        $factory = new IntrospectionProcessorFactory();
+        $handler = $factory($options);
+        $this->assertInstanceOf(IntrospectionProcessor::class, $handler);
+    }
+}
